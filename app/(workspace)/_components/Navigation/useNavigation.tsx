95b3d0cda67d2a6d79964d1f218644ea88e8d973
Gabriel Vargas Bravo <gabriel.vargas.bravo@gmail.com>
2023-10-17 10:19:15 -0500

    fix: useEffect missing dependency

--- app/(workspace)/_components/Navigation/useNavigation.tsx
+++ app/(workspace)/_components/Navigation/useNavigation.tsx
@@ -86,6 +86,7 @@ export function useNavigation(): INavigationReturn {
     } else {
       resetWidth();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isMobile]);
 
   useEffect(() => {
